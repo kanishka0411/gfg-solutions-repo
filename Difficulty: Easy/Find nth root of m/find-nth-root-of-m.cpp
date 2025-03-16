aa442f52b293f415f3b70f3eb8b1fa1d028514fb
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-03-17 01:06:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find nth root of m/find-nth-root-of-m.cpp
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.cpp
@@ -4,31 +4,47 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int nthRoot(int n, int m) {
-        int st=1;
-        int end=m;
-        while(st<=end){
-            int mid=st+(end-st)/2;
-            int power=1;
-            for(int i=1;i<=n;i++){
-                power*=mid;
-                if(power>m){
-                    break;
-                }
-            }
-            
-            if(power==m){
-                return mid;
-            }
-            if(power>m){
-                end=mid-1;
-            }else{
-                st=mid+1;
-            }
+
+  
+    int func(int x,int n,int m){
+        // returns 1 if ==m
+        // returns 0 if <m
+        // return 2 if >m
+        long long ans=1;
+        for(int i=1;i<=n;i++){
+            ans*=x;
+            if(ans>m) return 2;
         }
-        return -1;
+        if(ans==m) return 1;
+        
+        return 0;
+    }
+    int nthRoot(int n, int m) {
+        
+        //-------------------------------Brute--------------------------------------
+    //   for(int i=1;i<=m;i++){
+    //       int val=func(i,n);
+    //       if(val==m){
+    //           return i;
+    //       }
+    //   }
+    //   return -1;
+       
+       
+       //---------------------------------Optimal----------------------------------
+       int low=1;
+       int high=m;
+       while(low<=high){
+           int mid=(low+high)/2;
+           int midN=func(mid,n,m);
+           if(midN==1) return mid;
+           else if(midN==2) high=mid-1;
+           else low=mid+1;
+       }
+       return -1;
     }
 };
 
