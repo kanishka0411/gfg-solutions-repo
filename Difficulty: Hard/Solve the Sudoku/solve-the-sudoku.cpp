08cbbdbc92474b8ef5990e0114dc6f6a77d7d523
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-05-25 20:20:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -1,112 +1,43 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    bool isValidMove(vector<vector<int>> &board, int row, int col, int num, 
-                 vector<int> &rowMask, vector<int> &colMask, vector<int> &boxMask) {
-    if ((rowMask[row] & (1 << num)) || 
-        (colMask[col] & (1 << num)) || 
-        (boxMask[row / 3 * 3 + col / 3] & (1 << num))) {
-        return false;
-    }
-    return true;
-}
-
-bool sudokuSolverRec(vector<vector<int>> &board, int row, int col, 
-                     vector<int> &rowMask, vector<int> &colMask, vector<int> &boxMask) {
-    int size = board.size();
-
-    if (row == size - 1 && col == size) 
+    // Function to find a solved Sudoku.
+    bool isSafe(vector<vector<int>>&mat,int row,int col,int num){
+        for(int i=0;i<9;i++){
+            if(mat[row][i]==num) return false;
+        }
+        for(int j=0;j<9;j++){
+            if(mat[j][col]==num) return false;
+        }
+        
+        int srow=3*(row/3);
+        int scol=3*(col/3);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(mat[srow+i][scol+j]==num) return false;
+            }
+        }
+        
         return true;
-    if (col == size) {
-        row++;
-        col = 0;
     }
-
-    if (board[row][col] != 0) 
-        return sudokuSolverRec(board, row, col + 1, rowMask, colMask, boxMask);
-
-    for (int num = 1; num <= size; num++) {
-        if (isValidMove(board, row, col, num, rowMask, colMask, boxMask)) {
-            board[row][col] = num;
-            rowMask[row] |= (1 << num);
-            colMask[col] |= (1 << num);
-            boxMask[row / 3 * 3 + col / 3] |= (1 << num);
-
-            if (sudokuSolverRec(board, row, col + 1, rowMask, colMask, boxMask))
-                return true;
-
-            board[row][col] = 0;
-            rowMask[row] &= ~(1 << num);
-            colMask[col] &= ~(1 << num);
-            boxMask[row / 3 * 3 + col / 3] &= ~(1 << num);
-        }
-    }
-
-    return false;
-}
-
-void solveSudoku(vector<vector<int>> &board) {
-    int size = board.size();
-    vector<int> rowMask(size, 0), colMask(size, 0), boxMask(size, 0);
-
-    for (int row = 0; row < size; row++) {
-        for (int col = 0; col < size; col++) {
-            if (board[row][col] != 0) {
-                rowMask[row] |= (1 << board[row][col]);
-                colMask[col] |= (1 << board[row][col]);
-                boxMask[(row / 3) * 3 + col / 3] |= (1 << board[row][col]);
+    bool solve(vector<vector<int>>&mat){
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                if(mat[row][col]==0){
+                    for(int i=1;i<=9;i++){
+                        if(isSafe(mat,row,col,i)){
+                            mat[row][col]=i;
+                            if(solve(mat)) return true;
+                            mat[row][col]=0;
+                        }
+                    }
+                    return false;
+                }
             }
         }
+        return true;
     }
-
-    sudokuSolverRec(board, 0, 0, rowMask, colMask, boxMask);
-}
-
+    void solveSudoku(vector<vector<int>> &mat) {
+        solve(mat);
+        
+    }
 };
-
-//{ Driver Code Starts.
-
-vector<int> inputLine() {
-    string str;
-    getline(cin, str);
-    stringstream ss(str);
-    int num;
-    vector<int> res;
-    while (ss >> num) {
-        res.push_back(num);
-    }
-    return res;
-}
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<vector<int>> grid;
-        for (int i = 0; i < 9; i++) {
-            vector<int> v = inputLine();
-            grid.push_back(v);
-        }
-
-        Solution ob;
-
-        ob.solveSudoku(grid);
-
-        for (auto v : grid) {
-            for (auto elem : v) {
-                cout << elem << " ";
-            }
-            cout << endl;
-        }
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
