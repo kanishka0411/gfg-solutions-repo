c290572cf7c4c2da6c0d9e187e012223638bbc74
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-06-08 21:34:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.cpp
+++ Difficulty: Easy/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.cpp
@@ -1,37 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    Node *next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node *node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-void loopHere(Node *head, Node *tail, int position) {
-    if (position == 0)
-        return;
-
-    Node *walk = head;
-    for (int i = 1; i < position; i++)
-        walk = walk->next;
-    tail->next = walk;
-}
-
-
-// } Driver Code Ends
 // User function Template for C++
 
 /*struct Node
@@ -45,68 +11,23 @@ void loopHere(Node *head, Node *tail, int position) {
 
 */
 class Solution {
-public:
+  public:
     Node* findFirstNode(Node* head) {
-        if (head == nullptr || head->next == nullptr) {
-            return nullptr; // No cycle if the list is empty or has only one node
-        }
-
-        Node* slow = head;
-        Node* fast = head;
-
-        // Detect the cycle
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            // If slow and fast meet, there is a cycle
-            if (slow == fast) {
-                // Move slow to head and keep fast at meeting point
-                slow = head;
-                while (slow != fast) {
-                    slow = slow->next;
-                    fast = fast->next;
+        Node *fast=head;
+        Node *slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
                 }
-                return slow; // Return the first node of the cycle
+                return slow;
             }
         }
-
-        return nullptr; // No cycle found
+        return NULL;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        int k;
-        cin >> k;
-        cin.ignore();
-        assert(k <= arr.size());
-        struct Node *head = new Node(arr[0]);
-        struct Node *tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            tail->next = new Node(arr[i]);
-            tail = tail->next;
-        }
-        loopHere(head, tail, k);
-
-        Solution ob;
-        Node *ans = ob.findFirstNode(head);
-        cout << (ans == NULL ? -1 : ans->data) << "\n";
-        cout << "~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
