883f1f8b0c75e3d76d37a41dee45d1cabadbde20
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-08-16 06:32:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     int countFreq(vector<int>& arr, int target) {
@@ -15,72 +7,36 @@ class Solution {
         int first=-1;
         int last=-1;
         while(st<=end){
-            int mid=st+(end-st)/2;
+            int mid=(st+end)/2;
             if(arr[mid]==target){
                 first=mid;
                 end=mid-1;
-            }else if(arr[mid]<target){
-                st=mid+1;
-            }else{
+            }
+            else if(arr[mid]>target){
                 end=mid-1;
+            }else{
+                st=mid+1;
             }
         }
         
         st=0;
         end=n-1;
         while(st<=end){
-            int mid=st+(end-st)/2;
+            int mid=(st+end)/2;
             if(arr[mid]==target){
                 last=mid;
                 st=mid+1;
-            }else if(arr[mid]<target){
+            }
+            
+            else if(arr[mid]<target){
                 st=mid+1;
             }else{
                 end=mid-1;
             }
         }
-        if(first==-1){
-            return 0;
-        }
         
+        if(first==-1) return 0;
         return last-first+1;
         
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.countFreq(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
