286617d339a62626a4fae55c1f6deda16ee71a59
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2025-10-09 19:22:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome/palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool isPalindrome(int n) {
+        int num=n;
+        int sum=0;
+        while(num>0){
+            sum*=10;
+            sum+=num%10;
+            num/=10;
+        }
+        return sum==n;
+    }
+};
