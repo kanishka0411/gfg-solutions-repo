de2d45a843ae5ee1d09e1a6369887edb75430665
Kanishka <143314565+kanishka0411@users.noreply.github.com>
2026-01-02 23:46:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -1,44 +1,45 @@
 /*
-struct Node
-{
+class Node {
+  public:
     int data;
     Node* left;
     Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 */
+
 class Solution {
   public:
-    // Function to return a list of nodes visible from the top view
-    // from left to right in Binary Tree.
     vector<int> topView(Node *root) {
-        vector<int>ans;
-        if(root==nullptr) return ans;
-        map<int,int>mpp;
-        queue<pair<Node*,int>>q;
-        q.push({root,0});
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            Node *node=it.first;
-            int line=it.second;
-            if(mpp.find(line)==mpp.end()){
-                mpp[line]=node->data;
-            }
-            if(node->left!=nullptr){
-                q.push({node->left,line-1});
-            }
-            
-            if(node->right!=nullptr){
-                q.push({node->right,line+1});
-            }
-        }
-        
-        for(auto it:mpp){
-            ans.push_back(it.second);
-        }
-        
-        return ans;
-        
+       queue<pair<Node*,int>>q;
+       map<int,int>mpp;
+       q.push({root,0});
+       while(!q.empty()){
+           auto it=q.front();
+           q.pop();
+           Node *curr=it.first;
+           int line=it.second;
+           if(mpp.find(line)==mpp.end()){
+               mpp[line]=curr->data;
+           }
+           
+           if(curr->left){
+               q.push({curr->left,line-1});
+           }
+           if(curr->right){
+               q.push({curr->right,line+1});
+           }
+       }
+       vector<int>ans;
+       for(auto it:mpp){
+           ans.push_back(it.second);
+       }
+       return ans;
         
     }
 };
