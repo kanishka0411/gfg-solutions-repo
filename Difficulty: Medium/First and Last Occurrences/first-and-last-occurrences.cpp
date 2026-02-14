82665b04f66798454b7cb4ece50affcddb0886b8
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-02-14 12:16:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
@@ -3,18 +3,30 @@ class Solution {
     vector<int> find(vector<int>& arr, int x) {
         int first=-1;
         int last=-1;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==x){
-                first=i;
-                break;
+        int st=0;
+        int end=arr.size()-1;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(arr[mid]==x){
+                first=mid;
+                end=mid-1;
             }
-            
+            if(arr[mid]>=x) end=mid-1;
+            else st=mid+1;
         }
-        for(int i=arr.size()-1;i>=0;i--){
-            if(arr[i]==x){
-                last=i;
-                break;
+        
+        st=0;
+        end=arr.size()-1;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(arr[mid]==x){
+                last=mid;
+                st=mid+1;
             }
+            if(arr[mid]<=x) st=mid+1;
+            else end=mid-1;
+            
+            
         }
         return {first,last};
         
