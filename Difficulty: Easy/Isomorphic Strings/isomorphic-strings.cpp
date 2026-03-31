3a2ee16e423b6735a8a2ef1634de5a698d09aa84
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-03-31 10:52:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Isomorphic Strings/isomorphic-strings.cpp
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.cpp
@@ -1,17 +1,19 @@
 class Solution {
   public:
     bool areIsomorphic(string &s1, string &s2) {
-       int s[256]={0},t[256]={0};
-       int n=s1.size();
-       for(int i=0;i<n;i++){
-           if((s[s1[i]]!=0 || t[s2[i]]!=0) && (s[s1[i]]!=s2[i]+1 || t[s2[i]]!=s1[i]+1)){
-               return false;
-           }
-           s[s1[i]]=s2[i]+1;
-           t[s2[i]]=s1[i]+1;
-       }
-       
-       return true;
+        int n=s1.size();
+        int s[256]={0};
+        int t[256]={0};
+        for(int i=0;i<n;i++){
+            if((s[s1[i]]!=0 || t[s2[i]]!=0) && (s[s1[i]]!=s2[i]+1 || t[s2[i]]!=s1[i]+1)){
+                return false;
+            }
+            
+            s[s1[i]]=s2[i]+1;
+            t[s2[i]]=s1[i]+1;
+        }
+        
+        return true;
         
     }
 };
