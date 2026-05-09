909267c93f82e7f7e5158e53c93f32846b4e682a
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-05-10 00:37:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum path sum/maximum-path-sum.cpp
+++ Difficulty: Medium/Maximum path sum/maximum-path-sum.cpp
@@ -1,4 +1,5 @@
 /*
+Definition for Node
 class Node {
 public:
     int data;
@@ -7,24 +8,24 @@ public:
 
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = right = nullptr;
     }
 };
 */
+
 class Solution {
   public:
-    int dfs(Node *root,int &sum){
-        if(root==nullptr) return 0;
-        int lh=max(0,dfs(root->left,sum));
-        int rh=max(0,dfs(root->right,sum));
-        
-        sum=max(sum,lh+rh+root->data);
+    int find(Node *root,int &maxi){
+        if(!root) return 0;
+        int lh=max(0,find(root->left,maxi));
+        int rh=max(0,find(root->right,maxi));
+        maxi=max(maxi,lh+rh+root->data);
         return max(lh,rh)+root->data;
     }
     int findMaxSum(Node *root) {
-        int sum=INT_MIN;
-        dfs(root,sum);
-        return sum;
+       int maxi=INT_MIN;
+       find(root,maxi);
+       return maxi;
         
     }
 };
