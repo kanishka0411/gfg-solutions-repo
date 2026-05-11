2129ddc8eb451c397e4a3695ed9c231986ad0a1a
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-05-11 23:09:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Deletion at the end of a Linked List/deletion-at-the-end-of-a-linked-list.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* removeLastNode(Node* head) {
+        if(head==nullptr || head->next==nullptr) return nullptr;
+        Node *temp=head;
+        while(temp->next->next!=nullptr){
+            temp=temp->next;
+        }
+        delete(temp->next);
+        temp->next=nullptr;
+        return head;
+        
+    }
+};
