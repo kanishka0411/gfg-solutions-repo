a95d08c6b8dd30a3cc3df3b1e262797d9394dbf4
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-05-12 00:57:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -1,48 +1,51 @@
-/*  Node is defined as
-  struct Node {
+/* Node is defined as
+  class Node {
+  public:
     int data;
-    struct Node *next;
+    Node* next;
+
     Node(int x) {
         data = x;
-        next = NULL;
+        next = nullptr;
     }
-};*/
+};
+*/
 class Solution {
   public:
     Node* segregate(Node* head) {
-        if(head==NULL || head->next==NULL) return head;
-        Node* zeroHead=new Node(-1);
-        Node* oneHead=new Node(-1);
-        Node* twoHead=new Node(-1);
+        if(head==nullptr || head->next==nullptr) return head;
+        Node *zeroHead=new Node(-1);
+        Node *oneHead=new Node(-1);
+        Node *twoHead=new Node(-1);
         
         Node *zero=zeroHead;
         Node *one=oneHead;
         Node *two=twoHead;
-        Node *temp=head;
         
-        while(temp!=NULL){
+        Node *temp=head;
+        while(temp!=nullptr){
             if(temp->data==0){
                 zero->next=temp;
-                zero=temp;
+                zero=zero->next;
             }else if(temp->data==1){
                 one->next=temp;
-                one=temp;
-            }else if(temp->data==2){
+                one=one->next;
+            }else{
                 two->next=temp;
-                two=temp;
+                two=two->next;
             }
             temp=temp->next;
         }
-        
         zero->next=(oneHead->next)?oneHead->next:twoHead->next;
         one->next=twoHead->next;
-        two->next=NULL;
+        two->next=nullptr;
         
         Node *newHead=zeroHead->next;
-        delete zeroHead;
-        delete oneHead;
-        delete twoHead;
+        delete(zeroHead);
+        delete(oneHead);
+        delete(twoHead);
         return newHead;
+        
         
     }
 };
