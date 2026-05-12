4ffa89296008d5611b860f8b8bc66674e37e113e
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-05-12 11:29:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -2,45 +2,41 @@ class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         //build the adjancy list first
-        vector<vector<pair<int,int>>>adj(V);
-        
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0]; //start node
-            int v=edges[i][1]; //target node
-            int weight=edges[i][2];
-            
-            adj[u].push_back({v,weight});
-            adj[v].push_back({u,weight});
-        }
-        
-        //===========================
-        set<pair<int,int>>st;
-        vector<int>dist(V,1e9);
-        st.insert({0,src});
-        dist[src]=0;
-        
-        while(!st.empty()){
-            auto it=*(st.begin());
-            int distance=it.first;
-            int node=it.second;
-            st.erase(it);
-            
-            for(auto it:adj[node]){
-                int adjNode=it.first;
-                int edWg=it.second;
-                
-                if(distance+edWg<dist[adjNode]){
-                    //erase if it existed
-                    if(dist[adjNode]!=1e9){
-                        st.erase({dist[adjNode],adjNode});
-                    }
-                    
-                    dist[adjNode]=distance+edWg;
-                    st.insert({dist[adjNode],adjNode});
-                }
-            }
-        }
-        return dist;
+       vector<vector<pair<int,int>>>adj(V);
+       int n=edges.size();
+       for(int i=0;i<n;i++){
+           int u=edges[i][0];
+           int v=edges[i][1];
+           int w=edges[i][2];
+           
+           adj[u].push_back({v,w});
+           adj[v].push_back({u,w});
+       }
+       
+       set<pair<int,int>>st;
+       st.insert({0,src});
+       vector<int>dist(V,1e9);
+       dist[src]=0;
+       
+      while(!st.empty()){
+          auto it=*(st.begin());
+          int distance=it.first;
+          int node=it.second;
+          st.erase(it);
+          
+          for(auto it:adj[node]){
+              int adjNode=it.first;
+              int egW=it.second;
+              if(distance+egW<dist[adjNode]){
+                  if(dist[adjNode]!=1e9){
+                      st.erase({dist[adjNode],adjNode});
+                  }
+                  dist[adjNode]=distance+egW;
+                  st.insert({dist[adjNode],adjNode});
+              }
+          }
+      }
+      return dist;
         
     }
 };
