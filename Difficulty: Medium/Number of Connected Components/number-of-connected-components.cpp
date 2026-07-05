46e6177ee60d9b2ae155064e5ff47a89c212ff72
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-07-05 22:44:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    void bfs(int node,vector<int>adj[],vector<int>&vis){
+        vis[node]=1;
+        queue<int>q;
+        q.push(node);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:adj[node]){
+                if(vis[it]!=1){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+    }
+    int countConnected(int V, vector<vector<int>>& edges) {
+        vector<int>adj[V];
+        int e=edges.size();
+        for(int i=0;i<e;i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+            vector<int>vis(V,0);
+            int cnt=0;
+            for(int i=0;i<V;i++){
+                if(!vis[i]){
+                    cnt++;
+                    bfs(i,adj,vis);
+                }
+            }
+        
+        return cnt;
+        
+    }
+};
